cec7f35c91736ebb1e2e0f626ce0e5acfda52e70
nathanael42 <nathancox42@yahoo.com>
2022-05-24 19:20:26 -0500

    Added a mouseover event, added flex to center the pad, and changed background color

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
 const containerDiv = document.querySelector("#container");
-
+const gridItem = document.getElementById('gridItem');
 function makeRows(rows, cols) {
     containerDiv.style.setProperty('--grid-rows', rows);
     containerDiv.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
-      let cell = document.createElement("div");
-      cell.innerText = (i + 1);
-      container.appendChild(cell).className = "grid-item";
+      let cube = document.createElement("div");
+      container.appendChild(cube).className = "gridItem";
+      cube.addEventListener("mouseover", () => {
+          cube.style.backgroundColor = 'black';
+      });
     };
-  };
+};
   
-  makeRows(16, 16);
+makeRows(16, 16);
+  
+
+
