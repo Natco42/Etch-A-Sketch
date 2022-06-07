f16cbb0ef981622b74efeeffc38c53bd3c840249
nathanael42 <nathancox42@yahoo.com>
2022-06-07 17:54:41 -0500

    Added a reset button and user input to determine the pixel size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
+
+
+//creates grid
 const containerDiv = document.querySelector("#container");
 const gridItem = document.getElementById('gridItem');
 function makeRows(rows, cols) {
+
+
+  while (document.querySelector("button") !== null) {
+    document.querySelector("button").remove();
+  }
     containerDiv.style.setProperty('--grid-rows', rows);
     containerDiv.style.setProperty('--grid-cols', cols);
+    containerDiv.style.width = "900px";
+    containerDiv.style.height = "900px";
+    containerDiv.style.overflow = "hidden";
     for (i = 0; i < (rows * cols); i++) {
       let cube = document.createElement("div");
       container.appendChild(cube).className = "gridItem";
@@ -10,8 +21,30 @@ function makeRows(rows, cols) {
           cube.style.backgroundColor = 'black';
       });
     };
+  resetButton ();
 };
-  
+
+function resetButton () {
+  const buttonDiv = document.querySelector("#buttonDiv");
+  const resetButton = document.createElement("button");
+  resetButton.textContent = "Reset Grid";
+  resetButton.style.margin = "20px";
+  buttonDiv.appendChild(resetButton);
+
+  resetButton.addEventListener('click', () => {
+    document.querySelectorAll(".gridItem").forEach(e => e.remove());
+    let userInput = prompt("Please enter a number between 1 and 100.");
+    if (userInput > 100) {
+      alert("I'm sorry you've messed up the Emperor's groove. Please try again.");
+      return;
+    }
+    rows = userInput;
+    cols = userInput;
+    makeRows (rows, cols);
+  })
+
+
+}
 makeRows(16, 16);
   
 
